d96584214538bb9701a55533ebf3eca100f442cc
juani550 <78047975+juani550@users.noreply.github.com>
2021-02-04 17:22:12 -0300

    Add files via upload

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,17 +1,63 @@
 /*1.	Para el departamento de facturación:
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
-B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
+B.	Ingresar tres precios de productos y mostrar el suma de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+    var precio1;
+    var precio2;
+    var precio3;
+    var suma;
+
+
+    precio1 = parseFloat(txtIdPrecioUno.value);
+    precio2 = parseFloat(txtIdPrecioDos.value);
+    precio3 = parseFloat(txtIdPrecioTres.value);
+
+    suma = precio1 + precio2 + precio3;
+    
+    alert("la suma es $ "+ suma);
+
+
+    
 }
 function Promedio () 
 {
-	
+    var precio1;
+    var precio2;
+    var precio3;
+    var promedio;
+
+
+    precio1 = parseFloat(txtIdPrecioUno.value);
+    precio2 = parseFloat(txtIdPrecioDos.value);
+    precio3 = parseFloat(txtIdPrecioTres.value);
+
+    promedio = precio1 + precio2 + precio3;
+    
+    alert("el promedio es $ "+ promedio / 3);
+    
+
 }
 function PrecioFinal () 
 {
-	
+    
+    var precio1;
+    var precio2;
+    var precio3;
+    var suma;
+    var preciofinal;
+
+
+
+    precio1 = parseFloat(txtIdPrecioUno.value);
+    precio2 = parseFloat(txtIdPrecioDos.value);
+    precio3 = parseFloat(txtIdPrecioTres.value);
+
+    suma = (precio1 + precio2 + precio3);
+    
+    alert("El precio final con IVA es $ "+ (suma * 21 / 100) );
+
+
 }
--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -7,12 +7,63 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 function Rectangulo () 
 {
 
+    var largoString;
+    var anchoString;
+    
+    var largonumero;
+    var anchonumero;
+
+    var perimetro;
+    var alambre
+
+
+    largoString = txtIdLargo.value;
+    anchoString = txtIdAncho.value;
+
+    largonumero = parseFloat(largoString);
+    anchonumero = parseFloat(anchoString);
+
+    perimetro = (largonumero*2) + (anchonumero*2);
+
+    alambre = perimetro *3;
+
+    alert("la cantidad de alambre a comprar es " +alambre);
+
+
 }
+
+
+/*
+Mostrar la cantidad de alambre a comprar si se ingresara el radio de un terreno circular
+ y se debe alambra con tres hilos de alambre.
+ */
+
 function Circulo () 
 {
-	
+    var radio;
+    var perimetro;
+    var alambre;
+
+    radio = parseFloat(txtIdRadio.value);
+
+    perimetro = 2 * 3.14 * radio;
+
+    alambre = perimetro *3;
+
+    alert("la cantidad de alambre a comprar es " +alambre);
+    
+
+
+
+    
+    
+
+
 }
 function Materiales () 
 {
-	
+    
+    
+
+
 }
